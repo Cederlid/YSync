72a6c465e4cbf39fcdc987a1c880e6eec9115c1f
Wijdan Cederlid <hardrock_17@hotmail.com>
2024-02-20 16:35:25 +0100

    done MisMatchAction and modified DeleteFileAction

--- src/main/java/dev/ytterate/ysync/DeleteFileAction.java
+++ src/main/java/dev/ytterate/ysync/DeleteFileAction.java
@@ -1,24 +1,16 @@
 package dev.ytterate.ysync;
 
 import java.io.File;
-import java.nio.file.Files;
 
 public record DeleteFileAction(String file) implements SyncAction {
     @Override
     public void run() {
-        deleteFileInSource(new File(file));
+        deleteFileInSource(file);
     }
-    private void deleteFileInSource(File sourceFile) {
-        if (sourceFile.isDirectory()) {
-            File [] contents = sourceFile.listFiles();
-            if (contents != null){
-                for (File f : contents){
-                    if (! Files.isSymbolicLink(f.toPath())){
-                        deleteFileInSource(f);
-                    }
-                }
-            }
-            sourceFile.delete();
+    private void deleteFileInSource(String file) {
+        File fileToDelete = new File(file);
+        if (fileToDelete.exists()){
+            fileToDelete.delete();
         }
     }
     @Override
--- src/main/java/dev/ytterate/ysync/MisMatchAction.java
+++ src/main/java/dev/ytterate/ysync/MisMatchAction.java
@@ -1,10 +1,14 @@
 package dev.ytterate.ysync;
 
-public record MisMatchAction() implements SyncAction {
+import java.io.File;
+
+public record MisMatchAction(File file, File directory) implements SyncAction {
     @Override
     public void run() {
-
+        String.format("%s is a file in %s and a directory in %s!\n", file.getPath(), directory.getPath());
     }
+
+
 
     @Override
     public String render() {
