9ac30f9d6c007eaae68162f6861aa953857b192a
Wijdan Cederlid <hardrock_17@hotmail.com>
2024-03-28 14:28:44 +0100

    Added remote debug

--- Command Line/run_script.sh
+++ Command Line/run_script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+DEBUG_PORT=5005
+JAVA_DEBUG_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=$DEBUG_PORT"
 
 java -ea \
      -Dfile.encoding=UTF-8 \
      -Dsun.stdout.encoding=UTF-8 \
      -Dsun.stderr.encoding=UTF-8 \
+     $JAVA_DEBUG_OPTS \
      -classpath "/Users/wijdancederlid/Ytterate/ysync/out/production/Command Line:/Users/wijdancederlid/Ytterate/ysync/out/production/Library:/Users/wijdancederlid/.m2/repository/commons-io/commons-io/2.15.1/commons-io-2.15.1.jar:/Users/wijdancederlid/.m2/repository/org/json/json/20231013/json-20231013.jar:/Users/wijdancederlid/.m2/repository/com/beust/jcommander/1.82/jcommander-1.82.jar:/Users/wijdancederlid/Library/Application Support/JetBrains/Toolbox/apps/IDEA-U/ch-0/233.13135.103/IntelliJ IDEA.app/Contents/lib/idea_rt.jar" \
      dev.ytterate.ysync.cmd.Main \
      "$@"
